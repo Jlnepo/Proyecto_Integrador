822c86badf3dca3560ea2370759cdef61a9de918
Juan Nepomuceno <jlnepomucenoh1492@gmail.com>
2019-08-10 13:01:27 -0500

    Timer + ISQR

--- IRQ/Debug/synergy/ssp/src/bsp/mcu/all/bsp_delay.d
+++ IRQ/Debug/synergy/ssp/src/bsp/mcu/all/bsp_delay.d
@@ -1,137 +1,137 @@
 synergy/ssp/src/bsp/mcu/all/bsp_delay.o: \
  ../synergy/ssp/src/bsp/mcu/all/bsp_delay.c \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp/bsp_api.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\inc\ssp_common_api.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\inc\ssp_version.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\board\s7g2_sk\bsp.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\board\s7g2_sk\bsp_init.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\board\s7g2_sk\bsp_leds.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\board\s7g2_sk\bsp_qspi.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\board\s7g2_sk\bsp_ethernet_phy.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\inc\ssp_common_api.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_locking.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_irq.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\driver\api/r_ioport_api.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\inc\ssp_features.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\inc\ssp_common_api.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\driver\instances/r_fmi.h \
- C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\driver\api/r_fmi_api.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_delay.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_feature.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h \
- c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp/bsp_api.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\inc\ssp_common_api.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\inc\ssp_version.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\board\s7g2_sk\bsp.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\board\s7g2_sk\bsp_init.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\board\s7g2_sk\bsp_leds.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\board\s7g2_sk\bsp_qspi.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\board\s7g2_sk\bsp_ethernet_phy.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\inc\ssp_common_api.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_locking.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_irq.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\driver\api/r_ioport_api.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\inc\ssp_features.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\inc\ssp_common_api.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\driver\instances/r_fmi.h \
+ C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\driver\api/r_fmi_api.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_delay.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_feature.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h \
+ c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h \
  ../synergy/ssp/src/bsp/mcu/all/bsp_delay.h
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp/bsp_api.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp/bsp_api.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\inc\ssp_common_api.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\inc\ssp_common_api.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\inc\ssp_version.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\inc\ssp_version.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\board\s7g2_sk\bsp.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\board\s7g2_sk\bsp.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\board\s7g2_sk\bsp_init.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\board\s7g2_sk\bsp_init.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\board\s7g2_sk\bsp_leds.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\board\s7g2_sk\bsp_leds.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\board\s7g2_sk\bsp_qspi.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\board\s7g2_sk\bsp_qspi.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\board\s7g2_sk\bsp_ethernet_phy.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\board\s7g2_sk\bsp_ethernet_phy.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\inc\ssp_common_api.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\inc\ssp_common_api.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_locking.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_locking.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_irq.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_irq.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\driver\api/r_ioport_api.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\driver\api/r_ioport_api.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\inc\ssp_features.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\inc\ssp_features.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\inc\ssp_common_api.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\inc\ssp_common_api.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\driver\instances/r_fmi.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\driver\instances/r_fmi.h:
 
-C:\Users\SE_01\e2_studio\workspace1\IRQ\synergy\ssp\inc\driver\api/r_fmi_api.h:
+C:\Users\SE_01\git\Proyecto_Integrador1\IRQ\synergy\ssp\inc\driver\api/r_fmi_api.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_delay.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_delay.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_feature.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_feature.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h:
 
-c:\users\se_01\e2_studio\workspace1\irq\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h:
+c:\users\se_01\git\proyecto_integrador1\irq\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h:
 
 ../synergy/ssp/src/bsp/mcu/all/bsp_delay.h:
--- IRQ/src/synergy_gen/hal_data.h
+++ IRQ/src/synergy_gen/hal_data.h
@@ -6,14 +6,21 @@
 #include "common_data.h"
 #include "r_icu.h"
 #include "r_external_irq_api.h"
+#include "r_gpt.h"
+#include "r_timer_api.h"
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 /* External IRQ on ICU Instance. */
 extern const external_irq_instance_t g_external_irq0;
-#ifndef button_callback_SW4
-void button_callback_SW4(external_irq_callback_args_t *p_args);
+#ifndef button_callback_SW5
+void button_callback_SW5(external_irq_callback_args_t *p_args);
+#endif
+/** Timer on GPT Instance. */
+extern const timer_instance_t g_timer0;
+#ifndef NULL
+void NULL(timer_callback_args_t *p_args);
 #endif
 void hal_entry(void);
 void g_hal_init(void);
